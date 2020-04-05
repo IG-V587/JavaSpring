85dde4bb4ec26227d2e2256cbb54cc8073ea7482
梁光 <liangguangtop@163.com>
2020-04-05 23:02:47 +0800

    生命周期：BeanPostProcessor 后置处理器，实现该接口，进行初始化之后和销毁的方法

---
+++ src/main/java/com/test/condition/MyBeanPostProcessor.java
@@ -0,0 +1,23 @@
+package com.test.condition;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanPostProcessor;
+import org.springframework.stereotype.Component;
+
+/**
+ * 后置处理器，在bean 初始化前后进行
+ */
+@Component
+public class MyBeanPostProcessor implements BeanPostProcessor {
+    @Override
+    public Object postProcessBeforeInitialization(Object bean, String beanName) throws BeansException {
+        System.out.println("postProcessBeforeInitialization.."+bean+"====>"+beanName);
+        return bean;
+    }
+
+    @Override
+    public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
+        System.out.println("postProcessAfterInitialization.."+bean+"====>"+beanName);
+        return bean;
+    }
+}
--- src/main/java/com/test/config/MainConfigOfLifeCycle.java
+++ src/main/java/com/test/config/MainConfigOfLifeCycle.java
@@ -17,8 +17,14 @@ import org.springframework.context.annotation.Configuration;
  *  2、通过让bean实现 InitializingBean (定义初始化逻辑)
  *                   DisposableBean(定义销毁逻辑)
  *  3、可以使用JSR250:
- * @PostConstrust: 在bean 创建完成并且属性赋值完成，进行初始化方法
- * @PreDestroy: 在容器销毁bean之前通知我们进行清理工作
+ *              @PostConstrust: 在bean 创建完成并且属性赋值完成，进行初始化方法
+ *              @PreDestroy: 在容器销毁bean之前通知我们进行清理工作
+ *  4、BeanPostProcessor 【接口】: bean的后置处理器
+ *      在bean初始化前后进行一些处理工作
+ *      postProcessBeforeInitialization：在初始化之前工作
+ *      postProcessAfterInitialization：在初始化之后都乖乖女座
+ *
+ *
  *
  * 构造（对象创建）
  *      单实例：在容器启动的时候创建对象
@@ -29,7 +35,7 @@ import org.springframework.context.annotation.Configuration;
  *      单例：容器关闭的时候进行销毁
  *      多例：容器不会管理这个bean，容器不会调用销毁方法
  */
-@ComponentScan("com.test.entity")
+@ComponentScan({"com.test.entity","com.test.condition"})
 @Configuration
 public class MainConfigOfLifeCycle {
 
